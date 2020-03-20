7c946db715cf06e97e437f70b27813bda84c024c
Pooi lai Choong <choongpl@gmail.com>
2020-03-20 15:20:09 +1300

    first commit

---
+++ client/src/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Person extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1> This is an external class component</h1>
+      </div>
+    )
+  }
+}
+export default Person
---
+++ client/src/index.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Person from './Person';
+
+ReactDOM.render(<h1>Learning more react concepts</h1>,document.getElementById('root'));
+
+const heading =<h1>Learning more react concepts</h1>
+
+ReactDOM.render(heading, document.getElementById('root'));
+
+const headings =(
+  <div>
+    <h1>Learning more react concept</h1>
+    <h2>more tags can go inside the parent container, must be enclose within parentheses</h2>
+  </div>
+)
+
+ReactDOM.render(headings, document.getElementById('root1'));
+
+class Displayheadings extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Learning more react concept from Displayheadings class component</h1>
+        <h2>more tags can go inside the parent container, must be enclose within parentheses</h2>
+      </div>
+
+    )
+  }
+
+}
+
+ReactDOM.render(<Displayheadings />,document.getElementById('root2'));
+
+// function printHeadings(){
+//   return(
+//     <div>
+//       <h1>Learning more react concept from Displayheadings class component</h1>
+//       <h2>more tags can go inside the parent container, must be enclose within parentheses</h2>
+//     </div>
+//   )
+// }
+//
+// ReactDOM.render(<Displayheadings />,document.getElementById('root3'));
+
+
+ReactDOM.render(<Person/>,document.getElementById('root4'));
+
+//give input as props for the class
+class Displayname extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Hi, {this.props.name} Learning more react concepts from Displaheading class component</h1>
+        <h2>more tage go inside the parent container, must be enclosed within parentheses</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Displayname name="David"/>,document.getElementById('root5'));
+
+//class wth constructor
+
+class Mystate extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      firstName:"David",
+      secondName:"Smith"
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+handleClick(){
+  alert('green button is clicked');
+}
+
+  render(){
+    return(
+      <div>
+        <h1 className="blue"> {this.props.name} {this.props.age}</h1>
+        <button className="green" onClick={this.handleClick}>Click me </button>
+      </div>
+
+    )
+    // return(
+    //   <div>
+    //     <h1>{this.state.firstName}</h1>
+    //     <h2>{this.state.secondName}</h2>
+    //
+    //   </div>
+    // )
+  }
+}
+ReactDOM.render(<Mystate name="Daniel" age="43"/>,document.getElementById('root5'));
+
+//calling a function with
+
+function clickEvents(){
+  alert('i am clicked');
+}
+
+const myElement = (
+  <button onClick={clickEvents}> click here </button>
+
+)
+
+ReactDOM.render(myElement, document.getElementById('root7'));
